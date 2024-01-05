0a62f93a376f631f5bd900a68d47572112ef2f54
Mandeep Taneja <mandeeptaneja21@outlook.com>
2024-01-05 19:33:34 +0530

    Add uvicorn to main

--- main.py
+++ main.py
@@ -10,4 +10,5 @@ app = FastAPI()
 app.include_router(NoteRouter, prefix="/api/v1")
 app.include_router(UserRouter, prefix="/api/v1")
 
-uvicorn.run(app, host="0.0.0.0", port=8080, log_level="debug")
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8081, log_level="debug")
