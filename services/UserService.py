bace1dfab5a12f27eae54f1afbc027e17d442ca9
Mandeep Taneja <mandeeptaneja21@outlook.com>
2024-01-05 22:24:26 +0530

    Revert "Add paseto, switch to hardcoding, config issues right now add the jwt files to backup"
    This reverts commit aae414c41d802c491f7ca0a14b3a53292746dd5c.

--- services/UserService.py
+++ services/UserService.py
@@ -1,20 +1,21 @@
+import os
+
 from pymongo import MongoClient
 from fastapi import HTTPException, Depends
 from pymongo.errors import DuplicateKeyError
 from models.User import UserCreate, UserLogin
 from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
 import bcrypt
-import paseto
-from paseto.keys.symmetric_key import SymmetricKey
-from paseto.protocols.v4 import ProtocolVersion4
 
 class UserService:
     def __init__(self, mongo_client: MongoClient):
-        self.SECRET_KEY = SymmetricKey.generate(protocol=ProtocolVersion4)
-        self.ACCESS_TOKEN_EXPIRE_MINUTES = 30
+        self.SECRET_KEY = str(os.getenv("TOKEN_SECRET_KEY"))
+        self.ALGORITHM = str(os.getenv("TOKEN_GEN_ALGORITHM"))
+        self.ACCESS_TOKEN_EXPIRE_MINUTES = str(os.getenv("TOKEN_EXPIRY_DURATION_MINUTES"))
         self.client = mongo_client
-        self.db = self.client["NotesApp"]
-        self.collection = self.db["users"]
+        self.db = self.client[str(os.getenv("DB_NAME"))]
+        self.collection = self.db[str(os.getenv("DB_COLLECTION"))]
         self.collection.create_index([("username", 1), ("email", 1)], unique=True)
 
     def create_user_service(self, body: UserCreate):
@@ -32,29 +33,21 @@ class UserService:
         body.pop('password')
         return {**body}
 
-    def create_paseto_token(self, body):
-        token = paseto.create(key=self.SECRET_KEY,
-                              purpose='local',
-                              claims={"username": body["username"]},
-                              exp_seconds=300)
-        return token
+    def create_jwt_token(self, body):
+        return jwt.encode(body, key=self.SECRET_KEY, algorithm=self.ALGORITHM)
 
-    def decode_paseto_token(self, token: str):
+    def decode_jwt_token(self, token: str):
         try:
-            claims = paseto.parse(
-                key=self.SECRET_KEY,
-                purpose='local',
-                token=token,
-            )
-            print(claims)
-        except :
+            payload = jwt.decode(token, key=self.SECRET_KEY, algorithms=[self.ALGORITHM])
+            return payload
+        except JWTError:
             raise HTTPException(status_code=401, detail="Invalid credentials")
 
     def get_current_user(self, token: str = Depends(OAuth2PasswordBearer(tokenUrl="token"))):
         credentials_exception = HTTPException(
             status_code=401, detail="Could not validate credentials"
         )
-        return self.decode_paseto_token(token)
+        return self.decode_jwt_token(token)
 
     def login_user_service(self, body):
         body = body.model_dump()
@@ -68,7 +61,7 @@ class UserService:
             raise HTTPException(status_code=400, detail="Invalid Password")
 
         token_data = {"username": body["username"]}
-        token = self.create_paseto_token(token_data)
+        token = self.create_jwt_token(token_data)
         return {"access_token": token}
 
     def get_user_by_username(self, username):
