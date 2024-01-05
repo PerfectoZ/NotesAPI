2a2c08cb607b6c8124ddde58f86edd2bd4d4a542
Mandeep Taneja <mandeeptaneja21@outlook.com>
2024-01-05 18:56:35 +0530

    Add files via upload

--- test/test_connections.py
+++ test/test_connections.py
@@ -1,16 +1,13 @@
+import os
 import unittest
 from pymongo import MongoClient
 
 class TestDBConnection(unittest.TestCase):
     def test_connection(self):
-        client = MongoClient("mongodb://localhost:27017/")
-        self.assertIsNotNone(client)
-        db = client["NotesApp"]
-        self.assertIsNotNone(db)
-        collection = db["notes"]
-        self.assertIsNotNone(collection)
-        sequence = db["counters"]
-        self.assertIsNotNone(sequence)
+        client = MongoClient(os.getenv("DB_URL"), connectTimeoutMS=100, serverSelectionTimeoutMS=100)
+        ping = client.admin.command('ping')
+        self.assertIn("ok", ping)
+        self.assertEqual(ping["ok"], 1)
 
 if __name__ == '__main__':
     unittest.main()
